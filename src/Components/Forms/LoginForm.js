80ffe8aff8e0ae5515e84d7aef393b49f8e1a844
Aadithyan S <aadithyan.s@rubyians.com>
2022-04-18 18:29:45 +0530

    Integrating the login api

--- src/Components/Forms/LoginForm.js
+++ src/Components/Forms/LoginForm.js
@@ -1,6 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import useForm from "../../Validators/useForm";
+import { useNavigate } from "react-router-dom";
+import validate from "../../Validators/LoginFormValidation";
+import provider from "../../Services/provider";
 
 function LoginForm() {
+  const [authError, setAuthError] = useState("");
+  const history = useNavigate();
+  const { values, errors, handleChange, handleSubmit } = useForm(
+    handleLogin,
+    validate
+  );
+
+  function handleLogin() {
+    const session = { email: values.email, password: values.password };
+    provider
+      .post("/api/v1/sessions", { session })
+      .then((response) => {
+        localStorage.setItem("User", JSON.stringify(response.data));
+        history("/dashboard");
+      })
+      .catch((error) => {
+        console.log(error.response.data.data.message);
+        setAuthError(error.response.data.data.message);
+        console.log(authError);
+      });
+  }
+
   return (
     <div className="auth-wrapper">
       <div className="auth-content">
@@ -11,37 +37,63 @@ function LoginForm() {
           <span className="r"></span>
         </div>
         <div className="card">
-          <div className="card-body text-center">
-            <div className="mb-4">
-              <i className="feather icon-unlock auth-icon"></i>
-            </div>
-            <h3 className="mb-4">Login</h3>
-            <div className="input-group mb-3">
-              <input type="email" className="form-control" placeholder="Email" />
-            </div>
-            <div className="input-group mb-4">
-              <input
-                type="password"
-                className="form-control"
-                placeholder="password"
-              />
-            </div>
-            <div className="form-group text-left">
-              <div className="checkbox checkbox-fill d-inline">
-                <input
-                  type="checkbox"
-                  name="checkbox-fill-1"
-                  id="checkbox-fill-a1"
-                  checked=""
-                />
-                <label for="checkbox-fill-a1" className="cr">
-                  {" "}
-                  Save Details
-                </label>
+          <form onSubmit={handleSubmit} noValidate>
+            <div className="card-body text-center">
+              <div className="mb-4">
+                <i className="feather icon-unlock auth-icon"></i>
               </div>
+              <h3 className="mb-4">Login</h3>
+              <div className="input-group mb-3">
+                <input
+                  type="email"
+                  className="form-control"
+                  name="email"
+                  placeholder="Email"
+                  onChange={handleChange}
+                  value={values.email || ""}
+                  required
+                />
+              </div>
+              {errors.email && (
+                <div className="form-group text-left">
+                  <div className="input-group mb-3">
+                    <label className="text-danger mb-1">{errors.email}</label>
+                  </div>
+                </div>
+              )}
+              <div className="input-group mb-4">
+                <input
+                  type="password"
+                  className="form-control"
+                  name="password"
+                  placeholder="password"
+                  onChange={handleChange}
+                  value={values.password || ""}
+                  required
+                />
+              </div>
+
+              {errors.password && (
+                <div className="form-group text-left">
+                  <div className="input-group mb-3">
+                    <label className="text-danger mb-1">
+                      {errors.password}
+                    </label>
+                  </div>
+                </div>
+              )}
+
+              {authError && (
+                <div className="form-group text-left">
+                  <div className="input-group mb-3">
+                    <label className="text-danger mb-1">{authError}</label>
+                  </div>
+                </div>
+              )}
+
+              <button className="btn btn-primary shadow-2 mb-4">Login</button>
             </div>
-            <button className="btn btn-primary shadow-2 mb-4">Login</button>
-          </div>
+          </form>
         </div>
       </div>
     </div>
